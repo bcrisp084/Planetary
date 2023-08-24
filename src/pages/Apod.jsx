f77b3557c67bcc1c516c0468835c68121c293cdb
Brian Crisp <bcrisp084@yahoo.com>
2023-08-24 18:09:16 -0400

    updated dependencies/added apod page

--- src/pages/Apod.jsx
+++ src/pages/Apod.jsx
@@ -1,9 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import dotenv from "dotenv";
+
 const Apod = () => {
+  const data = useLoaderData();
   return (
     <>
       <h1>APOD</h1>
     </>
   );
+};
+
+export const apodLoader = async () => {
+  const res = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}`
+  );
+  return await res.json();
 };
 
 export default Apod;
